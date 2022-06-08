fe77916dd5466a4b38028b809048207724906fb4
GabrielTascaV <gabriel.villa@edu.pucrs.br>
2022-06-08 16:54:15 -0300

    Voltando para o menu ao cair

--- Scripts/Node2D.gd
+++ Scripts/Node2D.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 onready var cam = $Camera2D
 onready var actor = $Player
+onready var sceneLimit = $SceneLimit
 
 func _physics_process(_delta):
 	cam.position.x = actor.position.x-365
 	cam.position.y = actor.position.y-200
+	
+	if actor.position.y > sceneLimit.position.y:
+		get_tree().change_scene("res://Scenes/Menu.tscn")
 	pass
