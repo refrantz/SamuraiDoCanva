844a8ece9905b7e4578d6f3a95f418e28658fbeb
refrantz <renan262001@gmail.com>
2022-07-04 20:56:59 -0300

    ajustes level

--- Scripts/Level2.gd
+++ Scripts/Level2.gd
@@ -6,5 +6,6 @@ onready var sceneLimit = $SceneLimit
 onready var winLimit = $bannerWin/WinArea/WinLimit
 
 func _physics_process(_delta):
-	cam.position.x = actor.position.x-200
-	cam.position.y = actor.position.y-385
+	cam.position.x = actor.position.x-365
+	cam.position.y = actor.position.y-300
+	
